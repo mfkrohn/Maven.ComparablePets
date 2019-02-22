ce8d8d518f06965bbe43d704c8ea6852c36bbe09
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-22 09:25:16 -0500

    DO YOUR TEST(S) MIKE

--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -1,5 +1,32 @@
 package io.zipcoder;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApplicationTest {
+
+    @Test
+    public void howManyPetsTest(){
+        //Given
+        Pet cat1 = new Cat("Snuggles");
+        Pet dog1 = new Dog("BigDog");
+        Pet dino1 = new Dinosaur("Meatasaurus");
+        List<Pet> myPets = new ArrayList<Pet>();
+        myPets.add(cat1);
+        myPets.add(dog1);
+        myPets.add(dino1);
+        Integer expected = 3;
+
+        //When
+        Integer actual = myPets.getNumberOfPets();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
